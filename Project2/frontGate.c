0d181f4737aecf716541fb3f7ae2d4a93af1e33b
fx37 <mr.tofuman37@gmail.com>
2015-11-12 20:39:16 -0500

    Tweaks to readme and programs

--- Project2/frontGate.c
+++ Project2/frontGate.c
@@ -9,6 +9,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define GROUP_PORT 6000
+#define GROUP_IP "239.0.0.1"
+
 struct device{
   int id;
   char *type;
@@ -292,11 +295,60 @@ void *connection_handler(void *socket_desc){
   return 0;
 }
 
+void* multicast_listener(){
+  struct sockaddr_in addr;
+  int addrlen, sock, cnt;
+  struct ip_mreq mreq;
+  char message[50];
+
+  /* set up socket */
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock < 0) {
+    perror("socket");
+    exit(1);
+  }
+  bzero((char *)&addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(GROUP_PORT);
+  addrlen = sizeof(addr);
+  
+  if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {        
+
+    perror("bind");
+    exit(1);
+  }    
+  mreq.imr_multiaddr.s_addr = inet_addr(GROUP_IP);         
+  mreq.imr_interface.s_addr = htonl(INADDR_ANY);         
+  if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+		 &mreq, sizeof(mreq)) < 0) {
+    perror("setsockopt mreq");
+    exit(1);
+  }         
+  while (1) {
+    cnt = recvfrom(sock, message, sizeof(message), 0, 
+		   (struct sockaddr *) &addr, &addrlen);
+    if (cnt < 0) {
+      perror("recvfrom");
+      exit(1);
+    } else if (cnt == 0) {
+      break;
+    }
+    printf("%s: message = \"%s\"\n", inet_ntoa(addr.sin_addr), message);
+  }
+}
+
 int main(int argc , char *argv[])
 {
   //Add arguments
   readConfig(argv[1]);
 
+  pthread_t multi;
+  if (pthread_create(&multi,NULL,multicast_listener,NULL) < 0){
+    perror("Could not create new thread");
+    return 1;
+  }
+  
   int socket_desc , client_sock , c , read_size,*new_sock;
   struct sockaddr_in server , client;
      
--- Project2/sensor.c
+++ Project2/sensor.c
@@ -223,6 +223,10 @@ void identify(int gate_sock,char* command){
     return 0;
   }
 
+  //Create vector and Start multicast
+  if (strcmp(type,"clear")==0){
+
+  }
   if (strcmp(type,"switch")==0){
     s.state = !s.state;
     if(s.state){//ON
@@ -244,6 +248,49 @@ void identify(int gate_sock,char* command){
   }
 }
 
+void* multicast_listener(){
+  struct sockaddr_in addr;
+  int addrlen, sock, cnt;
+  struct ip_mreq mreq;
+  char message[50];
+
+  /* set up socket */
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock < 0) {
+    perror("socket");
+    exit(1);
+  }
+  bzero((char *)&addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(GROUP_PORT);
+  addrlen = sizeof(addr);
+  
+  if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {        
+
+    perror("bind");
+    exit(1);
+  }    
+  mreq.imr_multiaddr.s_addr = inet_addr(GROUP_IP);         
+  mreq.imr_interface.s_addr = htonl(INADDR_ANY);         
+  if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+		 &mreq, sizeof(mreq)) < 0) {
+    perror("setsockopt mreq");
+    exit(1);
+  }         
+  while (1) {
+    cnt = recvfrom(sock, message, sizeof(message), 0, 
+		   (struct sockaddr *) &addr, &addrlen);
+    if (cnt < 0) {
+      perror("recvfrom");
+      exit(1);
+    } else if (cnt == 0) {
+      break;
+    }
+    printf("%s: message = \"%s\"\n", inet_ntoa(addr.sin_addr), message);
+  }
+}
+
 int main(int argc , char *argv[])
 {
   int sock;
@@ -257,6 +304,13 @@ int main(int argc , char *argv[])
   if (f==NULL){
     printf("Error opening file\n");
     exit(1);
+  }
+
+  //Establish after clear
+  pthread_t multi;
+  if (pthread_create(&multi,NULL,multicast_listener,NULL) < 0){
+    perror("Could not create new thread");
+    return 1;
   }
   
   struct sockaddr_in server;
