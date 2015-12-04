3ea1f46a3ed1d44aa98724cad61e7c6bff7a0121
fx37 <mr.tofuman37@gmail.com>
2015-12-04 14:32:39 -0500

    added run.sh

---
+++ Project2/Outputs/run.sh
@@ -0,0 +1,11 @@
+gnome_terminal -e "bash -c \"../Outputs/Exe/frontGate ../Configs/frontGateConfig.txt GatewayOutput.log; exec bash\"" 
+sleep 1
+gnome_terminal -e "bash -c \"../Outputs/Exe/backGate ../Configs/backGateConfig.txt PersistentStorage.txt; exec bash\"" 
+sleep 1
+gnome_terminal -e "bash -c \"../Outputs/Exe/sensor ../Configs/doorConfig.txt ../Inputs/doorInput.txt DoorOutput.log; exec bash\"" 
+sleep 1
+gnome_terminal -e "bash -c \"../Outputs/Exe/sensor ../Configs/keyConfig.txt  ../Inputs/keyInput.txt KeychainOutput.log; exec bash\"" 
+sleep 1
+gnome_terminal -e "bash -c \"../Outputs/Exe/sensor ../Configs/motionConfig.txt ../Inputs/motionInput.txt MotionOutput.log; exec bash\"" 
+sleep 1
+gnome_terminal -e "bash -c \"../Outputs/Exe/device ../Configs/deviceConfig.txt SecurityDeviceOutput.log; exec bash\""
